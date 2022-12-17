8d06653a705eacc80cbc0b5684ebfadb4cbf18ed
Karim-Muhammad <kimoomar007@gmail.com>
2022-12-18 00:44:00 +0200

    add paths, dynamic

--- job/models.py
+++ job/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+# each type, helps us for these following:
+#    - make django choose suitable html widget for this type
+#    - validation
+#    - db size
+choices = [
+     ("part-time", "part-time"),
+     ("full-time", "full-time")
+]
+class Job(models.Model): # models.Model === Table
+     title = models.CharField(max_length=100) # === column
+     type = models.CharField(max_length=15, choices=choices)
+     salary = models.IntegerField(default=0);
+     vacancy = models.IntegerField(default=1)
+     description = models.TextField(max_length=1000);
+     category = models.ForeignKey("Category", models.CASCADE, default=1)
+
+     def __str__(self) -> str:
+          return self.title;
+
+class Category(models.Model):
+     name = models.CharField(max_length=20);
+
+     def __str__(self) -> str:
+          return self.name;
--- job/views.py
+++ job/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .models import Job;
 # Create your views here.
+
+def jobs_page(request):
+     jobs = Job.objects.all();
+     return render(request, "jobs.html", {"jobs": jobs})
+
+def job_info(request, id):
+     job = Job.objects.all()[id-1]
+     return render(request, "job.html", {"job": job})
